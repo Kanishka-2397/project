32190a64c7eca36996b9ec29d9e2daa43bdebef6
kanishka <kanishka9723@gmail.com>
2025-03-18 11:07:34 +0530

    Create cal.py
    first commit

---
+++ cal.py
@@ -0,0 +1,20 @@
+def add():
+    print("This is add cal")
+    a=int(input("enter your number1:"))
+    b=int(input("enter your number2:"))
+    print(a+b)
+def sub():
+    print("This is sub cal")
+    d=int(input("enter your num1:"))
+    f=int(input("enter your num2:"))
+    print(d-f)
+def mul():
+    print("This is mul cal")
+    a=int(input("enter your num1:"))
+    b=int(input("enter your num2:"))
+    print(a*b)
+def dev():
+    print("This is dev cal")
+    a=int(input("enter your num1:"))
+    b=int(input("enter your num2:"))
+    print(a%b)

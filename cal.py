71d7d06b93e2a869b43f358209e35cbed8e1119e
kanishka <kanishka9723@gmail.com>
2025-03-18 12:58:02 +0530

    Update function.py

--- cal.py
+++ cal.py
@@ -18,3 +18,44 @@ def dev():
     a=int(input("enter your num1:"))
     b=int(input("enter your num2:"))
     print(a%b)
+
+
+
+def findpassorfail(num):
+    if(num>35):
+        print("pass or even")
+    elif(num<35):
+        print("fail or odd")
+    else:
+        print("invoid num")
+
+findpassorfail(24)
+
+
+
+s_username="kanishka"
+s_password="123"
+uname=input("enter your uname:")
+passwd=input("enter your password:")
+def validate():
+    if(s_username==uname and s_password==passwd):
+        return ("true")
+    else:
+        return ("false")
+a=validate()
+print(a)
+
+
+
+def add(a,b):
+    return(a+b)
+a=int(input("enter the number a :"))
+b=int(input("enter the number b :"))
+c=int(input("enter the number c :"))
+
+added=add(a,b)
+output=added*c
+print(output)
+
+
+

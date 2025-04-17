7600d1ff0db8b72c5b0ab40b2c0b795bee09ef90
kanishka <kanishka9723@gmail.com>
2025-04-17 10:51:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,113 +1,163 @@
-provider "aws" {}
-resource "aws_vpc" "tf_vpc1"{
+provider "aws" {
+  region = "us-east-1"
+}
+
+# VPC
+resource "aws_vpc" "tf_vpc1" {
   cidr_block = "10.0.0.0/16"
-  tags ={
-    name = "tf_vpc1"
+  tags = {
+    Name = "tf_vpc1"
   }
 }
-resource "aws_subnet" "tf_sub-1"{
-  vpc_id = aws_vpc.tf_vpc1.id
+
+# Subnet 1
+resource "aws_subnet" "tf_sub-1" {
+  vpc_id            = aws_vpc.tf_vpc1.id
   availability_zone = "us-east-1a"
-  cidr_block = "10.0.1.0/24"
-  tags ={
-    name = "tf_sub-1"
+  cidr_block        = "10.0.1.0/24"
+  tags = {
+    Name = "tf_sub-1"
   }
 }
-resource "aws_subnet" "tf_sub-2"{
-  vpc_id = aws_vpc.tf_vpc1.id
+
+# Subnet 2
+resource "aws_subnet" "tf_sub-2" {
+  vpc_id            = aws_vpc.tf_vpc1.id
   availability_zone = "us-east-1b"
-  cidr_block = "10.0.2.0/24"
-  tags ={
-    name = "tf_sub-2"
+  cidr_block        = "10.0.4.0/24"
+  tags = {
+    Name = "tf_sub-2"
   }
 }
-resource "aws_route_table" "tf_route1"{
-  vpc_id = aws_vpc.tf_vpc1.id
-  route{
-    cidr_block ="0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw1.id
-  }
-  tags ={
-    name ="tf_route1"
-  }
 
-}
-resource "aws_route_table" "tf_route2"{
-  vpc_id = aws_vpc.tf_vpc1.id
-  route= []
-  tags ={
-    name ="tf_route2"
+# Internet Gateway
+resource "aws_internet_gateway" "igw1" {
+  tags = {
+    Name = "igw1"
   }
+}
 
-}
-resource "aws_route_table_association" "a"{
-  subnet_id =aws_subnet.tf_sub-2.id
-  route_table_id = aws_route_table.tf_route2.id
-}
-resource "aws_internet_gateway" "igw1"{
-  tags ={
-    name = "igw1"
-  }
-}
-resource "aws_internet_gateway_attachment" "igw_attachment"{
+# Attach IGW to VPC
+resource "aws_internet_gateway_attachment" "igw_attachment" {
   internet_gateway_id = aws_internet_gateway.igw1.id
   vpc_id              = aws_vpc.tf_vpc1.id
 }
-resource "aws_route_table_association" "tf_sub_association"{
-  subnet_id = aws_subnet.tf_sub-1.id
-  route_table_id = aws_route_table.tf_route1.id
+
+# Route Table 1 (Public)
+resource "aws_route_table" "tf-rt1" {
+  vpc_id = aws_vpc.tf_vpc1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw1.id
+  }
+
+  tags = {
+    Name = "tf-rt1"
+  }
 }
-resource "aws_security_group" "tf_sg1"{
-  vpc_id= aws_vpc.tf_vpc1.id
+
+# Route Table 2 (Private)
+resource "aws_route_table" "tf-rt2" {
+  vpc_id = aws_vpc.tf_vpc1.id
+  route  = []
+  tags = {
+    Name = "tf-rt2"
+  }
+}
+
+# Associate subnet-1 with public route table
+resource "aws_route_table_association" "demo_sub_1_association" {
+  subnet_id      = aws_subnet.tf_sub-1.id
+  route_table_id = aws_route_table.tf-rt1.id
+}
+
+# Associate subnet-2 with private route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.tf_sub-2.id
+  route_table_id = aws_route_table.tf-rt2.id
+}
+
+# Security Group
+resource "aws_security_group" "terra_sg1" {
+  vpc_id = aws_vpc.tf_vpc1.id
 
   ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "icmp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress{
-    from_port = 80
-    to_port   = 80
-    protocol  ="tcp"
-    cidr_blocks =["0.0.0.0/0"]
-  }
-  egress{
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    ipv6_cidr_blocks =["::/0"]
-  }
-  tags = {
-    name= "tf_sg1"
-      }
-}
-resource "aws_key_pair" "tf_keys"{
-  key_name = "tf_keys"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCzjX8fSdjOPA3ot/JnX+TMieItWHlphHy3ZFx6keGonQdsqgLoirSXpW9fNCnUj1qBK/J/nh2uNZIzHpl+w3wEnj81LrrSY3xqtab7QPrtOBl+mI9sMH9Nm2dAUwYjdD5eS6Y7rhvjtPTcKujfYsHufXynCCCvBwyiDSPM/EYYKUMwuWCM37dwH+gPIBJjNrqQIXSIeQtu4Tdg3H4XrJv44qPtGzdnOmpjH4iG68priwOBo4XMb/7CZjkrvH7RzRekYJEJOWoMeYUhMOS8rcKepqfjuoPjFnOwg0Ogk8U/POfDXfPzIbHHYjUPgA6px1CElsnwYgD6ST3ePZdrBs6rk7Am1eh3UfMTeWKNEFfzf7hMiESYpcMIbEM0yBDU5mtn1R3QhG02JpjbxwyPtNkaXH0tcDFcrxcizzgsSuc7uSunYGAazN9h/xEo03SOujVqi7w3U91vdttDZ2KB4Cm/8tWeUTeXq3TOoXC7FU/wWkFWUyZbMFgAHukkQ3DyBG0= sarav@kanishka"
-}
-resource "aws_instance" "server-1"{
-  ami = data.aws_ami.centOs1.id
-  instance_type = "t2.micro"
-  key_name = aws_key_pair.tf_keys.key_name
-  subnet_id= aws_subnet.tf_sub-1.id
-  security_groups =[aws_security_group.tf_sg1.id]
-  availability_zone = "us-east-1a"
-  tags ={
-    name ="server-1"
-  }
-}
-data "aws_ami""centOs1"{
-  most_recent = true 
-  owners      = ["amazon"]
 
-  filter{
-    name ="name"
-    values=["RHEL-9.4.0_HVM-20240605-x86_64-82-Hourly2-GP3"]
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  filter{
-    name = "virtualization-type"
-    values = ["hvm"]
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 50000
+    to_port     = 50000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    cidr_blocks       = ["0.0.0.0/0"]
+    ipv6_cidr_blocks  = ["::/0"]
+  }
+
+  tags = {
+    Name = "Terraform_SG"
   }
 }
+
+# EC2 Instances (Ubuntu)
+resource "aws_instance" "my_ec2_instance" {
+  count             = 2
+  ami               = "ami-084568db4383264d4" # Ubuntu 22.04 LTS in us-east-2
+  instance_type     = "t2.micro"
+  key_name          = "TFF" # Replace with your actual key name
+  subnet_id         = aws_subnet.tf_sub-1.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.terra_sg1.id]
+
+  tags = {
+    Name = "Ubuntu Server-${count.index + 1}"
+  }
+}
+
+# Outputs
+output "instance_public_ip" {
+  value = aws_instance.my_ec2_instance[*].public_ip
+}
+
+output "instance_id" {
+  value = aws_instance.my_ec2_instance[*].id
+}
+
